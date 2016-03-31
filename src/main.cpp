9c8913fcadd56de28a5e9456ce8a5c0ea0eb8324
Maria Robu <ucabmrr@ucl.ac.uk>
2016-03-31 14:55:57 +0100

    added openmp deploy files

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include "defs.h"
 #include "GameOfLife.h"
 #include <fstream>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 
 
 using namespace GameOfLifeNS;
@@ -10,13 +14,13 @@ int main() {
     std::cout << "***Start!\n";
     std::ofstream outStream("output.txt");
     if (outStream.is_open()) {
-        GameOfLife gof(55, 55);
+        GameOfLife gof(15, 15);
 //        GameOfLife gof(5, 5);
         gof.seedRandom();
 //        gof.seedBlinker();
         outStream << gof.getWidth() << " " << gof.getHeight() << "\n";
         gof.writeToFile(outStream);
-        while (gof.getFrame() < 50) {
+        while (gof.getFrame() < 20) {
             gof.updateGrid();
             gof.writeToFile(outStream);
         }
